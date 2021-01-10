0eddf35a50ead715c877313e88972f697110018f
Ivan <ivankzn@mail.ru>
2021-01-10 12:16:40 +0300

    'class and object - cat experiments'

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -1,45 +1,45 @@
 
-public class Loader
-{
-    public static void main(String[] args)
-    {
-        Cat cat = new Cat();
-// Created five cats and got theirs weights
-        Cat Murka  = new Cat();
-        System.out.println("Murka has weight:"+ Murka.getWeight());
-        Cat Vasja  = new Cat();
-        System.out.println("Vasja has weight:"+ Vasja.getWeight());
-        Cat Boris  = new Cat();
-        System.out.println("Boris has weight:"+ Boris.getWeight());
-        Cat Spring  = new Cat();
-        System.out.println("Spring has weight:"+ Spring.getWeight());
-        Cat Lucky  = new Cat();
-        System.out.println("Lucky has weight:"+ Lucky.getWeight());
+public class Loader {
+    //Fields
+
+    public static void main(String[] args) {
+// Parameters
+
+// Created five cats (five objects of class Cat) and got theirs weights
+        Cat murka = new Cat();
+        System.out.println("Murka has weight:" + murka.getWeight());
+        Cat vasja = new Cat();
+        System.out.println("Vasja has weight:" + vasja.getWeight());
+        Cat boris = new Cat();
+        System.out.println("Boris has weight:" + boris.getWeight());
+        Cat spring = new Cat();
+        System.out.println("Spring has weight:" + spring.getWeight());
+        Cat lucky = new Cat();
+        System.out.println("Lucky has weight:" + lucky.getWeight());
 
 // Fed Murka with 100 of chicken in the morning
-        Murka.feed(100.0);
-        System.out.println ("Murka has weight after morning feeding:" + Murka.getWeight());
+        murka.feed(100.0);
+        System.out.println("Murka has weight after morning feeding:" + murka.getWeight());
 // Fed Lucky with 300 of fish in the morning
-        Lucky.feed(300.0);
-        System.out.println ("Lucky has weight after morning feeding:" + Lucky.getWeight());
+        lucky.feed(300.0);
+        System.out.println("Lucky has weight after morning feeding:" + lucky.getWeight());
 
 // Fed Boris with a lot of beans.....Oh No. It has exploded. Poor Boris.
-        while ( Boris.getStatus() != "Exploded")
+        while (boris.getStatus() != "Exploded")
         {
-            Boris.feed(300.0);
+            boris.feed(300.0);
         }
-        System.out.println ("Boris has weight after morning feeding:" + Boris.getWeight());
-        System.out.println ("Boris has status after morning feeding:" + Boris.getStatus());
+        System.out.println("Boris has weight after morning feeding:" + boris.getWeight());
+        System.out.println("Boris has status after morning feeding:" + boris.getStatus());
 
 // Unlucky Vasja lost his weight to death after made a lof of meowing this evening
-        while ( Vasja.getStatus() != "Dead")
+        while (vasja.getStatus() != "Dead")
         {
-            Vasja.meow();
+            vasja.meow();
         }
-        System.out.println ("Vasja lost his weight after meowing this evening:" + Vasja.getWeight());
-        System.out.println ("Vasja has status after meowing this evening :" + Vasja.getStatus());
+        System.out.println("Vasja lost his weight after meowing this evening:" + vasja.getWeight());
+        System.out.println("Vasja has status after meowing this evening :" + vasja.getStatus());
         System.out.println("RIP Vasja and Boris. We will miss you.");
-
-        //System.out.println(cat.getStatus());
+       
     }
 }
