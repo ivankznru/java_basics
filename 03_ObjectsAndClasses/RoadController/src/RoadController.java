33933c4faa82691085fac4dfd777b2b0170d0e04
Ivan <ivankzn@mail.ru>
2021-01-20 22:03:07 +0300

    Road Controller lesson 6

--- 03_ObjectsAndClasses/RoadController/src/RoadController.java
+++ 03_ObjectsAndClasses/RoadController/src/RoadController.java
@@ -3,37 +3,36 @@ import core.Camera;
 
 import java.util.Scanner;
 
-public class RoadController
-{
+import java.util.Scanner;
+
+public class RoadController {
     private static double passengerCarMaxWeight = 3500.0; // kg
     private static int passengerCarMaxHeight = 2000; // mm
-    private static int controllerMaxHeight = 4000; // mm
+    private static int controllerMaxHeight = 3500; // mm
 
     private static int passengerCarPrice = 100; // RUB
     private static int cargoCarPrice = 250; // RUB
     private static int vehicleAdditionalPrice = 200; // RUB
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Сколько автомобилей сгенерировать?");
 
         Scanner scanner = new Scanner(System.in);
         int carsCount = scanner.nextInt();
 
-        for(int i = 0; i < carsCount; i++)
-        {
+        for (int i = 0; i < carsCount; i++) {
             Car car = Camera.getNextCar();
             System.out.println(car);
 
             //Пропускаем автомобили спецтранспорта бесплатно
-            if (car.isSpecial) {
+            if (car.isSpecial()) {
                 openWay();
                 continue;
             }
 
             //Проверяем высоту и массу автомобиля, вычисляем стоимость проезда
             int price = calculatePrice(car);
-            if(price == -1) {
+            if (price == -1) {
                 continue;
             }
 
@@ -44,33 +43,27 @@ public class RoadController
     /**
      * Расчёт стоимости проезда исходя из массы и высоты
      */
-    private static int calculatePrice(Car car)
-    {
-        int carHeight = car.height;
+    private static int calculatePrice(Car car) {
+        int carHeight = car.getHeight();
         int price = 0;
-        if (carHeight > controllerMaxHeight)
-        {
+        if (carHeight > getControllerMaxHeight()) {
             blockWay("высота вашего ТС превышает высоту пропускного пункта!");
             return -1;
-        }
-        else if (carHeight > passengerCarMaxHeight)
-        {
-            double weight = car.weight;
+        } else if (carHeight > getPassengerCarMaxHeight()) {
+            double weight = car.getWeight();
             //Грузовой автомобиль
-            if (weight > passengerCarMaxWeight)
-            {
-                price = passengerCarPrice;
-                if (car.hasVehicle) {
-                    price = price + vehicleAdditionalPrice;
+            if (weight > getPassengerCarMaxWeight()) {
+                price = getCargoCarPrice();
+                if (car.hasVehicle()) {
+                    price = price + getVehicleAdditionalPrice();
                 }
             }
             //Легковой автомобиль
             else {
-                price = cargoCarPrice;
+                price = getPassengerCarPrice();
             }
-        }
-        else {
-            price = passengerCarPrice;
+        } else {
+            price = getPassengerCarPrice();
         }
         return price;
     }
@@ -78,16 +71,62 @@ public class RoadController
     /**
      * Открытие шлагбаума
      */
-    private static void openWay()
-    {
+    private static void openWay() {
         System.out.println("Шлагбаум открывается... Счастливого пути!");
     }
 
     /**
      * Сообщение о невозможности проезда
      */
-    private static void blockWay(String reason)
-    {
+    private static void blockWay(String reason) {
         System.out.println("Проезд невозможен: " + reason);
+    }
+
+    public static double getPassengerCarMaxWeight() {
+        return passengerCarMaxWeight;
+    }
+
+    public static void setPassengerCarMaxWeight(double passengerCarMaxWeight) {
+        RoadController.passengerCarMaxWeight = passengerCarMaxWeight;
+    }
+
+    public static int getPassengerCarMaxHeight() {
+        return passengerCarMaxHeight;
+    }
+
+    public static void setPassengerCarMaxHeight(int passengerCarMaxHeight) {
+        RoadController.passengerCarMaxHeight = passengerCarMaxHeight;
+    }
+
+    public static int getControllerMaxHeight() {
+        return controllerMaxHeight;
+    }
+
+    public static void setControllerMaxHeight(int controllerMaxHeight) {
+        RoadController.controllerMaxHeight = controllerMaxHeight;
+    }
+
+    public static int getPassengerCarPrice() {
+        return passengerCarPrice;
+    }
+
+    public static void setPassengerCarPrice(int passengerCarPrice) {
+        RoadController.passengerCarPrice = passengerCarPrice;
+    }
+
+    public static int getCargoCarPrice() {
+        return cargoCarPrice;
+    }
+
+    public static void setCargoCarPrice(int cargoCarPrice) {
+        RoadController.cargoCarPrice = cargoCarPrice;
+    }
+
+    public static int getVehicleAdditionalPrice() {
+        return vehicleAdditionalPrice;
+    }
+
+    public static void setVehicleAdditionalPrice(int vehicleAdditionalPrice) {
+        RoadController.vehicleAdditionalPrice = vehicleAdditionalPrice;
     }
 }
--- 03_ObjectsAndClasses/RoadController/src/core/Car.java
+++ 03_ObjectsAndClasses/RoadController/src/core/Car.java
@@ -1,18 +1,56 @@
 package core;
 
-public class Car
-{
+public class Car {
     public String number;
     public int height;
     public double weight;
     public boolean hasVehicle;
     public boolean isSpecial;
 
-    public String toString()
-    {
-        String special = isSpecial ? "СПЕЦТРАНСПОРТ " : "";
+    public String toString() {
+        String special = isSpecial()? "СПЕЦТРАНСПОРТ " : "";
         return "\n=========================================\n" +
-            special + "Автомобиль с номером " + number +
-            ":\n\tВысота: " + height + " мм\n\tМасса: " + weight + " кг";
+                special + "Автомобиль с номером " + getNumber() +
+                ":\n\tВысота: " + getHeight() + " мм\n\tМасса: " + getWeight() + " кг";
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public boolean hasVehicle() {
+        return hasVehicle;
+    }
+
+    public void hasVehicle(boolean hasVehicle) {
+        this.hasVehicle = hasVehicle;
+    }
+
+    public boolean isSpecial() {
+        return isSpecial;
+    }
+
+    public void isSpecial(boolean special) {
+        isSpecial = special;
     }
 }
