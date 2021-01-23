27d772a73536a0b2c7d26c456696b680eaf77a04
Ivan <ivankzn@mail.ru>
2021-01-23 09:35:22 +0300

    MinMaxValueOfNumbers

---
+++ 04_NumbersStringsAndDates/homework_4.4/MinMaxValueOfNumbers/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(  "Минимальное значение byte:"  + Byte.MIN_VALUE );
+        System.out.println(  "Максимальное значение byte:"  + Byte.MAX_VALUE );
+        System.out.println(  "Минимальное значение short:"  + Short.MIN_VALUE );
+        System.out.println(  "Максимальное значение short:"  + Short.MAX_VALUE );
+        System.out.println(  "Минимальное значение int:"  + Integer.MIN_VALUE );
+        System.out.println(  "Максимальное значение int:"  + Integer.MAX_VALUE );
+        System.out.println(  "Минимальное значение long:"  + Long.MIN_VALUE );
+        System.out.println(  "Максимальное значение long:"  + Long.MAX_VALUE );
+        System.out.println(  "Минимальное значение float:"  + Float.MIN_VALUE );
+        System.out.println(  "Максимальное значение float:"  + Float.MAX_VALUE );
+        System.out.println(  "Минимальное значение double:"  + Double.MIN_VALUE );
+        System.out.println(  "Максимальное значение double:"  + Double.MAX_VALUE );
+
+    }
+}
