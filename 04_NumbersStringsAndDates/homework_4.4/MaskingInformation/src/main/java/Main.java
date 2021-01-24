e0f77b5b030604fc40d5ca33e8efe13e60c401ed
Ivan <ivankzn@mail.ru>
2021-01-24 12:10:02 +0300

    MaskingInformation

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -5,8 +5,37 @@ public class Main {
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        return text;
-    }
+        text.trim();
+       text.length();
+       placeholder.trim();
+       placeholder.length();
 
+       String firstPart =" ";
+       String secondPart=" ";
+       String thirdPart=" ";
+       int firstLess =text.indexOf('<');
+       int firstMore =text.indexOf('>');
+       int secondLess = text.lastIndexOf('<');
+       int secondMore = text.lastIndexOf('>');
+
+       if (firstLess>0 & firstMore>0 & firstLess>(text.length()-secondLess+2) ){
+            firstPart = text.substring(0,firstLess );
+            secondPart = text.substring(firstMore+1, secondLess);
+           thirdPart= text.substring( secondMore+1,text.length());
+           text= firstPart + placeholder + secondPart + placeholder +thirdPart;
+
+        }
+       if
+       (firstLess>0 & firstMore>0 & firstLess<=(text.length()-secondLess+2)) {
+            firstPart = text.substring(0,firstLess );
+            secondPart = text.substring(firstMore+1, text.length());
+            text = firstPart + placeholder + secondPart;
+
+        };
+        return text;
+
+    }
 }
+
+//String safe = searchAndReplaceDiamonds(«Номер кредитной карты <4008 1234 5678> 8912», «***»);
+//В safe должна содержаться строка «Номер кредитной карты *** 8912».
