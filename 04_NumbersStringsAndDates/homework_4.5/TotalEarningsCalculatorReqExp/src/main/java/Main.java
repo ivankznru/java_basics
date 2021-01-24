02172cf562752f2fee589884395cc95bd5495ac9
Ivan <ivankzn@mail.ru>
2021-01-24 21:53:32 +0300

    TotalEarningsCalculator

--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,23 @@
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  }
 
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
-  }
+    }
+
+
+    public static int calculateSalarySum(String text) {
+        int calculateSalarySum = 0;
+        String[] sentences = text.split("\\,");
+        for (int i = 0; i < sentences.length; i++) {
+            String salary = sentences[i].replaceAll("[^0-9]", " ").trim();
+            if (!salary.equals("")) {
+                calculateSalarySum = calculateSalarySum + Integer.parseInt(salary);
+            } else {
+                calculateSalarySum = 0;
+            }
+        }
+        return calculateSalarySum;
+    }
 
 }
