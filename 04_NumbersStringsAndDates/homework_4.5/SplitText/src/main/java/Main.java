ecbb403b8278534ec4f4a57ce1e1d5614571ef53
Ivan <ivankzn@mail.ru>
2021-01-30 10:56:52 +0300

    SplitText

--- 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
   }
 
- 
+  public static String splitTextInToWords(String text) {
+    String splitWord = "";
+    text = text.replaceAll("[^a-zA-Z’]", "\r");
+    text = text.replaceAll("\\s+", "\r");
+     text = text.replaceAll("\\s+$", "");
+    String[] words = text.split("''");
+    for (int i = 0; i < words.length; i++) {
+      splitWord= words[i].replaceAll("\\s", "\n");
+
+    }
+    return splitWord;
   }
 }
