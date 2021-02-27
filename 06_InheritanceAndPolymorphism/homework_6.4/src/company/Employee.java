12a0b8c7ccd06079d5c44dea7568fe2ef0b287b5
Ivan <ivankzn@mail.ru>
2021-02-27 19:50:01 +0300

    empty

--- 06_InheritanceAndPolymorphism/homework_6.4/src/company/Employee.java
+++ 06_InheritanceAndPolymorphism/homework_6.4/src/company/Employee.java
@@ -1,21 +1,4 @@
-package company;
 
-
-public interface Employee extends Comparable<Employee> {
-
-    Double getMonthSalary();
-
-    void setSalary();
-
-    void setZeroSalary();
-
-    @Override
-    default int compareTo(Employee employee) {
-        return getMonthSalary().compareTo(employee.getMonthSalary());
-    }
-
-
-}
 
 
 
