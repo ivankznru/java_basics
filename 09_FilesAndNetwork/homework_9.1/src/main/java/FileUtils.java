cbd17bd8c64165e6fc8f4273eff7761b1db7489a
Ivan <ivankzn@mail.ru>
2021-03-12 04:03:54 +0300

    homework_9.1

--- 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
@@ -1,6 +1,43 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
+
 public class FileUtils {
 
-    public static long calculateFolderSize(String path) {
-        return 0;
-    }
-}
+    public static long calculateFolderSize(String path) throws IOException {
+
+    Path folder = Paths.get(path);
+    long size = Files.walk(folder)
+            .filter(p -> p.toFile().isFile())
+            .mapToLong(p -> p.toFile().length())
+            .sum();
+        return size;
+                }
+
+//   File folder = new File(path);
+//   long size = org.apache.commons.io.FileUtils.sizeOfDirectory(folder);
+
+//      File folder = new File(path);
+
+//     Iterable<File> files = Files.fileTraverser().breadthFirst(folder);
+//      long size = StreamSupport.stream(files.spliterator(), false) .filter(f -> f.isFile())
+//             .mapToLong(File::length).sum();
+
+//      return size;
+//   }
+
+public static String getReadableSize_thenCorrect(long size) throws IOException {
+
+        String[] units = new String[] { "B", "KB", "MB", "GB", "TB" };
+        int unitIndex = (int) (Math.log10(size) / 3);
+        double unitValue = 1 << (unitIndex * 10);
+
+        String readableSize = new DecimalFormat("#,##0.#")
+        .format(size / unitValue) + " "
+        + units[unitIndex];
+        return readableSize;
+        }
+        }
+
--- 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
@@ -1,5 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static final Logger LOG = LogManager.getRootLogger();
     public static void main(String[] args) {
+        while (true) {
+            try {
+                System.out.println("Введите путь к папке:");
+                String path = new Scanner(System.in).nextLine();
+                if (path != null && !path.isEmpty()) {
+                    File folder = new File(path);
+                    if (!folder.exists())
+                        throw new FileNotFoundException();
+                    System.out.println(FileUtils.getReadableSize_thenCorrect(FileUtils.calculateFolderSize(path)));
+                }
+            } catch (NullPointerException npe) {
+                LOG.warn("В указанной папке нет файлов.");
+            } catch (FileNotFoundException sfe) {
+                LOG.error("Директория не существует. Указан ошибочный путь.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
