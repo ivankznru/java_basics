416d2a22a5166107ee12342e68742458d230bd00
User <ivankzn@mail.ru>
2021-07-17 12:38:06 +0300

    toDoList14_3_1

---
+++ 14_SpringBootWebDevelopment/ToDoList_14_3_1/src/main/java/main/model/Task.java
@@ -0,0 +1,56 @@
+package main.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String description;
+    private boolean done;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public void refreshTask(Task task) {
+        if (this.getId() == task.getId()) {
+            setDone(task.getDone());
+            setDescription(task.getDescription());
+            setName(task.getName());
+        }
+    }
+}
