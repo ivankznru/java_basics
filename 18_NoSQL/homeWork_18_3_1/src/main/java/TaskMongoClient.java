a4d26834bf431e0f2e593f66169b336bbd43ddbc
ivankznru <ivankzn@mail.ru>
2021-09-23 21:21:18 +0300

    homeWork_18_3_1

---
+++ 18_NoSQL/homeWork_18_3_1/src/main/java/TaskMongoClient.java
@@ -0,0 +1,16 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import lombok.Getter;
+
+@Getter
+public class TaskMongoClient {
+
+    private final MongoClient mongoClient;
+    private final MongoDatabase mongoDatabase;
+
+    public TaskMongoClient() {
+       this.mongoClient = new MongoClient("127.0.0.1", 27017);
+       this.mongoDatabase = mongoClient.getDatabase("local");
+    }
+
+}
