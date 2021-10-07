5c9712e400babccde68ecfb58d867addbdfb98ca
ivankznru <ivankzn@mail.ru>
2021-10-07 10:56:37 +0300

    hw_19_2

---
+++ 19_Performance/hw_19_2/VoteAnalyzer/src/main/java/handler/DOMHandler.java
@@ -0,0 +1,84 @@
+package handler;
+
+import model.Voter;
+import model.WorkTime;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashMap;
+
+public class DOMHandler {
+
+    private static DateTimeFormatter birthDayFormat = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+    private static SimpleDateFormat visitDateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+    private static HashMap<Integer, WorkTime> voteStationWorkTimes = new HashMap<>();
+    private static HashMap<Voter, Integer> voterCounts = new HashMap<>();
+
+    public static void duplicatedVotersDOM() {
+        for (Voter voter : voterCounts.keySet()) {
+            Integer count = voterCounts.get(voter);
+            if (count > 1) {
+                System.out.println(voter + " - " + count);
+            }
+        }
+    }
+
+    public static void workTimesVotingDOM() {
+        for (Integer votingStation : voteStationWorkTimes.keySet()) {
+            WorkTime workTime = voteStationWorkTimes.get(votingStation);
+            System.out.println("\t" + votingStation + " - " + workTime);
+        }
+    }
+
+    public static void parseFileDOM(String fileName) throws Exception {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new File(fileName));
+
+        findEqualVoters(doc);
+        fixWorkTimes(doc);
+    }
+
+    private static void findEqualVoters(Document doc) {
+        NodeList voters = doc.getElementsByTagName("voter");
+        int votersCount = voters.getLength();
+        for (int i = 0; i < votersCount; i++) {
+            Node node = voters.item(i);
+            NamedNodeMap attributes = node.getAttributes();
+
+            String name = attributes.getNamedItem("name").getNodeValue();
+            LocalDate birthDay = LocalDate.parse(attributes.getNamedItem("birthDay").getNodeValue(), birthDayFormat);
+
+            Voter voter = new Voter(name, birthDay);
+            Integer count = voterCounts.get(voter);
+            voterCounts.put(voter, count == null ? 1 : count + 1);
+        }
+    }
+
+    private static void fixWorkTimes(Document doc) throws Exception {
+        NodeList visits = doc.getElementsByTagName("visit");
+        int visitCount = visits.getLength();
+        for (int i = 0; i < visitCount; i++) {
+            Node node = visits.item(i);
+            NamedNodeMap attributes = node.getAttributes();
+
+            Integer station = Integer.parseInt(attributes.getNamedItem("station").getNodeValue());
+            Date time = visitDateFormat.parse(attributes.getNamedItem("time").getNodeValue());
+            WorkTime workTime = voteStationWorkTimes.get(station);
+            if (workTime == null) {
+                workTime = new WorkTime();
+                voteStationWorkTimes.put(station, workTime);
+            }
+            workTime.addVisitTime(time.getTime());
+        }
+    }
+}
